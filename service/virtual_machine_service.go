4c311f40cbe71a3a17439c976447422426287157
fitme96 <fitme96@gmail.com>
2023-09-15 06:08:25 +0000

    优化虚拟机生命周期管理及快照管理

--- service/virtual_machine_service.go
+++ service/virtual_machine_service.go
@@ -5,6 +5,74 @@ import (
 	"github.com/luthermonson/go-proxmox"
 )
 
+// 生命周期
+const (
+	DELETE_VM   = 201
+	STOP_VM     = 202
+	START_VM    = 203
+	SHUTDOWN_VM = 204
+	REBOOT_VM   = 205
+)
+
+func vmaction(action uint, v Vmcyclelife) error {
+	var err error
+	client := GetClient()
+	node, _ := client.Node(v.Node)
+	vm, _ := node.VirtualMachine(v.Vmid)
+	switch {
+	case action == DELETE_VM:
+		_, err = vm.Delete()
+	case action == STOP_VM:
+		_, err = vm.Stop()
+	case action == START_VM:
+		_, err = vm.Start()
+	case action == SHUTDOWN_VM:
+		_, err = vm.Shutdown()
+	case action == REBOOT_VM:
+		_, err = vm.Reboot()
+
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteVm(c *gin.Context, v Vmcyclelife) error {
+	if err := vmaction(DELETE_VM, v); err != nil {
+		return err
+	}
+	return nil
+}
+
+func VmStop(c *gin.Context, v Vmcyclelife) error {
+	if err := vmaction(STOP_VM, v); err != nil {
+		return err
+	}
+	return nil
+}
+
+func VmStart(c *gin.Context, v Vmcyclelife) error {
+	if err := vmaction(START_VM, v); err != nil {
+		return err
+	}
+	return nil
+}
+
+func VmShutdown(c *gin.Context, v Vmcyclelife) error {
+	if err := vmaction(SHUTDOWN_VM, v); err != nil {
+		return err
+	}
+	return nil
+}
+
+func VmReboot(c *gin.Context, v Vmcyclelife) error {
+	if err := vmaction(REBOOT_VM, v); err != nil {
+		return err
+	}
+	return nil
+}
+
 func CloneVm(c *gin.Context, vmclone Clonevm) (newvm int, err error) {
 
 	client := GetClient()
@@ -16,18 +84,6 @@ func CloneVm(c *gin.Context, vmclone Clonevm) (newvm int, err error) {
 	}
 	return newvm, nil
 
-}
-
-func DeleteVm(c *gin.Context, v Vmcyclelife) error {
-
-	client := GetClient()
-	node, _ := client.Node(v.Node)
-	vm, _ := node.VirtualMachine(v.Vmid)
-	if _, err := vm.Delete(); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func ResizeDisk(c *gin.Context, resize Resize) error {
@@ -53,62 +109,47 @@ func VmConfig(c *gin.Context, vmoption Configvm) error {
 	}
 	return nil
 }
-func VmStop(c *gin.Context, v Vmcyclelife) error {
-	client := GetClient()
-	node, _ := client.Node(v.Node)
-	if vm, err := node.VirtualMachine(v.Vmid); err != nil {
-		return err
-	} else {
-		vm.Stop()
-
-	}
-
-	return nil
-}
-
-func VmStart(c *gin.Context, v Vmcyclelife) error {
-	client := GetClient()
-	node, _ := client.Node(v.Node)
-	if vm, err := node.VirtualMachine(v.Vmid); err != nil {
-		return err
-	} else {
-		vm.Start()
-
-	}
-	return nil
-}
-
-func VmShutdown(c *gin.Context, v Vmcyclelife) error {
-	client := GetClient()
-	node, _ := client.Node(v.Node)
-	if vm, err := node.VirtualMachine(v.Vmid); err != nil {
-		return err
-	} else {
-		vm.Shutdown()
-	}
-	return nil
-}
-
-func VmReboot(c *gin.Context, v Vmcyclelife) error {
-	client := GetClient()
-	node, _ := client.Node(v.Node)
-	if vm, err := node.VirtualMachine(v.Vmid); err != nil {
-		return err
-	} else {
-		vm.Reboot()
-
-	}
-	return nil
-}
 
 // snapshot
-func NewSnapshot(c *gin.Context, s Snapshotvm) error {
+const (
+	CREATE_SNAPSHOT   = 100
+	ROLLBACK_SNAPSHOT = 101
+)
+
+func snapshotaction(action int, snap Snapshotvm) error {
+	var err error
 	client := GetClient()
-	node, _ := client.Node(s.Vm.Node)
-	vm, _ := node.VirtualMachine(s.Vm.Vmid)
-	if _, err := vm.NewSnapshot(s.SnapshotName); err != nil {
+	node, _ := client.Node(snap.Vm.Node)
+	vm, _ := node.VirtualMachine(snap.Vm.Vmid)
+
+	switch {
+	case action == CREATE_SNAPSHOT:
+		_, err = vm.NewSnapshot(snap.SnapshotName)
+
+	case action == ROLLBACK_SNAPSHOT:
+		_, err = vm.SnapshotRollback(snap.SnapshotName)
+
+	}
+	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func NewSnapshot(c *gin.Context, snap Snapshotvm) error {
+	if err := snapshotaction(CREATE_SNAPSHOT, snap); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SnapshotRollback(c *gin.Context, snap Snapshotvm) error {
+	if err := snapshotaction(ROLLBACK_SNAPSHOT, snap); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -122,15 +163,4 @@ func Snapshots(c *gin.Context, v Vmcyclelife) ([]*proxmox.Snapshot, error) {
 		return snapshots, nil
 	}
 
-}
-
-func SnapshotRollback(c *gin.Context, s Snapshotvm) error {
-	client := GetClient()
-	node, _ := client.Node(s.Vm.Node)
-	vm, _ := node.VirtualMachine(s.Vm.Vmid)
-	if _, err := vm.SnapshotRollback(s.SnapshotName); err != nil {
-		return err
-	}
-
-	return nil
 }
